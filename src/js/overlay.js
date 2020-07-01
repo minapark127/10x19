2083b9b4bcad5d09a081c41b52ff78b247ba0c3e
Mina <mina.park127@gmail.com>
2020-07-01 20:35:21 +1000

    added animation on overlay menu
    improved toggle on JS and added animation on opening/closing overlay menu and on text slide when menu is opened

--- src/js/overlay.js
+++ src/js/overlay.js
@@ -1,15 +1,36 @@
 const menuBtn = document.querySelectorAll(".js-menu");
 const overlayMenu = document.querySelector(".js-overlay");
+const overlayBody = overlayMenu.querySelector("section");
+const overlayMenuCL = overlayMenu.classList;
+
+const BROWN = "#b07833";
 
 function loadOverlay(event) {
-  if (overlayMenu.classList.contains("hiding")) {
-    overlayMenu.classList.remove("hiding");
-    // when open the menu after closing, scroll back to top
-    // not remained at the past position
-    overlayMenu.querySelector("section").scrollTo(0, 0);
-  } else {
-    overlayMenu.classList.add("hiding");
+  //replacing animation effect
+  if (event.target.innerHTML === "menu") {
+    overlayMenuCL.replace("scale-out-tr", "scale-in-tr");
+    overlayBody.style.color = "white";
+
+    //add text animation after overlay menu animation
+    overlayMenu.addEventListener(
+      "animationend",
+      overlayBody.classList.add("slide-in-bottom")
+    );
+  } else if (event.target.innerHTML === "close") {
+    //changing text colours for smoother animation
+    overlayBody.style.color = BROWN;
+    overlayMenuCL.replace("scale-in-tr", "scale-out-tr");
   }
+
+  //delay toggle - display none for animation effect
+  setTimeout(toggleHiding, 400);
+  function toggleHiding() {
+    overlayMenuCL.toggle("hiding");
+  }
+
+  // when open the menu after closing, scroll back to top
+  // not remaining at the past position
+  overlayBody.scrollTo(0, 0);
 }
 
 menuBtn.forEach((menuBtn) => {
