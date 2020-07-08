cbebbf2c2ac9cf3c0027c6a5069a429bd7d4e7dc
Mina <mina.park127@gmail.com>
2020-07-08 15:25:57 +1000

    added responsive imgae function
    improved js on img slider.
    added style on responsive image

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,19 +2,42 @@ const ul = document.querySelectorAll(".js-slider__ul");
 const placeholder = document.querySelector(".js-slider__placeholder");
 const img = placeholder.querySelector("img");
 
+const MOBILEMAXWIDTH = "(max-width: 767px)";
+
+const mobile = window.matchMedia(MOBILEMAXWIDTH);
+
+function makeImgContainer(text) {
+  const footer = document.querySelector("footer");
+  const section = document.createElement("section");
+  const imgResponsive = document.createElement("img");
+  section.appendChild(imgResponsive);
+  section.classList.add("responsive-img-container");
+  imgResponsive.classList.add("responsive-img-container__img");
+  imgResponsive.setAttribute("src", `src/images/${text}.jpg`);
+  document.querySelector(".wrapper").insertBefore(section, footer);
+  hideMain();
+}
+
+function hideMain() {
+  document.querySelector("main").classList.add("hiding");
+}
 function loadImg(text) {
   img.setAttribute("src", `src/images/${text}.jpg`);
 }
 
-function hoverHandler(event) {
+function getImgName(event) {
   const text = event.target.firstElementChild.innerHTML;
   const clearText = text.slice(1);
-  loadImg(clearText);
+  if (mobile.matches) {
+    makeImgContainer(clearText);
+  } else {
+    loadImg(clearText);
+  }
 }
 
 ul.forEach((ul) => {
   const link = ul.querySelectorAll("a");
   link.forEach((link) => {
-    link.addEventListener("mouseenter", hoverHandler);
+    link.addEventListener("mouseenter", getImgName);
   });
 });
